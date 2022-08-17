25d58831b0af78a60d4d24842aeb3e91bd217f6f
drwscefn <108461378+drwscefn@users.noreply.github.com>
2022-08-17 12:23:08 +0100

    Add files via upload

---
+++ lotl-portscan.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import socket
+host = "1.1.1.1"
+portList = [21,22,53,80,443,3306,8443,8080]
+for port in portList:
+ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ try:
+  s.connect((host,port))
+  print("Port ", port, " is open")
+ except:
+  print("Port ", port, " is closed")
